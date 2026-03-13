9d987c4a14f04d2f32997668c173465e7c172622
Jiayang Sun <sjyhehe@gmail.com>
2026-03-12 22:01:26 -0500

    Update dbc to v0.1.2

--- Formula/dbc.rb
+++ Formula/dbc.rb
@@ -2,8 +2,8 @@ class Dbc < Formula
   desc "Dollar Calculator CLI Tool"
   homepage "https://github.com/NoPointExc/dbc"
   url "https://github.com/NoPointExc/dbc/archive/refs/heads/master.tar.gz"
-  version "0.1.1" # Manually setting version since we're tracking main
-  sha256 "5962b18f9da2514ee5ebff9f41f89681e8cd5e15df58f91d9a292d6ab182a560"
+  version "0.1.2" # Manually setting version since we're tracking main
+  sha256 "812fa2cb136c818ee81ba4772e6751ee2524e82982c750a5eeab0fe6f0799034"
   license "MIT"
 
   depends_on "rust" => :build
